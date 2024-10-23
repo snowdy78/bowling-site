e44c9d3904064a03be470e08852cebd04877218e
snowdy78 <themrleoner12@mail.ru>
2024-10-23 19:58:54 +0700

    рефактор небольшой

--- php/header-profile-load.php
+++ php/header-profile-load.php
@@ -1,18 +1,30 @@
 <?php
     include_once "src/DataBase.php";
     session_start();
-    if (empty($_SESSION['login']))
+    // if user is not signed in
+    if (!isset($_SESSION['login']))
     {
-        echo "<div class='register-frame'><a class='header-button' href='/index.php?page=login'>Войти</a><a class='header-button' href='/index.php?page=register'>Регистрация</a></div>";
-    }  
-    else 
-    {
-        $log = $_SESSION['login'];
-        $db = new DataBase("localhost", "root", "", "test1", 3306);
-        $user = $db->getUser($log);
-        if (empty($user))
-            echo "user not found :(";
-        else 
-            echo "<div class='register-frame' style='grid-template-columns: 2fr'><a class='header-button' href='/index.php?page=profile'>".$user->data['login']."</a></div>";
+        echo "
+            <div class='register-frame'>
+                <a class='header-button' href='/index.php?page=login'>
+                    Войти
+                </a>
+                <a class='header-button' href='/index.php?page=register'>
+                    Регистрация
+                </a>
+            </div>";
+        return;
     }
+    $log = $_SESSION['login'];
+    $db = new DataBase("localhost", "root", "", "test1", 3306);
+    $user = $db->getUser($log);
+    if (!isset($user))
+        echo "user not found :(";
+    else 
+        echo "
+            <div class='register-frame' style='grid-template-columns: 2fr'>
+                <a class='header-button' href='/index.php?page=profile'>"
+                    .$user->data['login'].
+                "</a>
+            </div>";
 ?>
