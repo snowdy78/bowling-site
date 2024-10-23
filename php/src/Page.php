be75b2c5238a978a6c82ba8580257f1c92e38203
snowdy78 <themrleoner12@mail.ru>
2024-10-23 14:56:21 +0700

    фикс загрузки главной

--- php/src/Page.php
+++ php/src/Page.php
@@ -1,13 +1,13 @@
 <?php
     class Page 
     {
-        static public $pages = array("main", "about", "filial", "balls", "login", "order", "profile", "register", "roads", "turnir", "ordered");
+        public static $pages = ["main", "about", "filial", "balls", "login", "order", "profile", "register", "roads", "turnir", "ordered"];
         public $description = "";
-        public $title = "Document";
+        public $title = "Bowling";
         public $name;
-        public function __construct() 
+        public function __construct()
         {
-            $this->name = isset($_GET['page']) ? $_GET['page'] : $this->pages[0];
+            $this->name = isset($_GET['page']) ? $_GET['page'] : self::$pages[0];
             $this->title = $this->name;
         }
         public function display() 
@@ -16,15 +16,15 @@
             $this->loadContent();
             $this->loadFooter();
         }
-        public function loadHeader() 
+        private function loadHeader() 
         {
             require ('php/load-header.inc');
         }
-        public function loadContent() 
+        private function loadContent() 
         {
             include "php/pages/".$this->name.".php";
         }
-        public function loadFooter() 
+        private function loadFooter() 
         {
             require ('php/load-footer.inc');
         }
