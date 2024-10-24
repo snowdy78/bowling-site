5130fd2ede0b853cd349bc6e625412006448bb3b
snowdy78 <themrleoner12@mail.ru>
2024-10-24 16:14:04 +0700

    профиль изменен

--- php/pages/profile.php
+++ php/pages/profile.php
@@ -3,6 +3,34 @@
     <div class="container-fullscreen">
         <div class='text-placement'>
             <div id="profile" class="flex flexh-center column-flex-direction">
+                <?php
+                    include_once "./php/src/DataBase.php";
+                    
+                    $db = DataBase::getDataBase();
+                    $login = $_SESSION['login'];
+
+                    if (isset($login)) 
+                    {
+                        $user = $db->getUser($login);
+                        if (!empty($user))
+                        {
+                            echo "
+                                <div class='profile-container'>
+                                    <div class='profile-label'>
+                                        <span>login: </span><span>".$user->data["login"]."</span> 
+                                    </div>
+                                    <div class='profile-label'>
+                                        <span>phone: </span><span>".$user->data["phone_number"]."</span> 
+                                    </div>
+                                    <div class='profile-label'>
+                                        <span>email: </span><span>".$user->data["email"]."</span> 
+                                    </div>
+                                </div> 
+                            ";
+                        }
+                    }
+
+                ?>
                 <a class="logout-btn" href="./php/logout.php">
                     Log Out
                 </a>
